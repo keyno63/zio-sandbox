a0c929cde54a03492241f5d39a1dfc9f5995dc8c
Keyno <kofujiw@gmail.com>
2021-10-31 00:08:06 +0900

    from scala value

--- zio1/src/main/scala/com/github/keyno/sandbox/zio/overview/EffectSandbox.scala
+++ zio1/src/main/scala/com/github/keyno/sandbox/zio/overview/EffectSandbox.scala
@@ -1,7 +1,7 @@
 package com.github.keyno.sandbox.zio.overview
 
 import zio.console.{getStrLn, putStrLn}
-import zio.{Task, UIO, ZIO}
+import zio.{IO, Task, UIO, ZIO}
 
 object EffectSandbox extends zio.App {
   final def run(args: List[String]) =
@@ -43,5 +43,52 @@ class FromFailureValues {
 }
 
 class FromScalaValues {
+  // fron Option
+  def createZioOption() = {
+    ZIO.fromOption(Option(2))
+  }
+
+  def convertErrorToString(zioOption: IO[Option[Nothing], Int]) = {
+    zioOption.mapError(_ => "It wasn't there")
+  }
+
+  def sampleCode(): Unit = {
+    case class User(teamId: String)
+    case class Team()
+
+    val maybeId: IO[Option[Nothing], String] = ZIO.fromOption(Some("abc123"))
+    def getUser(userId: String): IO[Throwable, Option[User]] = ???
+    def getTeam(teamId: String): IO[Throwable, Team] = ???
+
+
+    // should convert to Any, Option[Throwable], (User, Team)
+    val result = (for {
+      id   <- maybeId
+      user <- getUser(id).some
+      team <- getTeam(user.teamId).asSomeError
+    } yield (user, team))
+  }
+
+  // from Either
+  def createFromEither(value: String) = {
+    ZIO.fromEither(Right(value))
+  }
+
+  // from Try
+  def createFromTry() = {
+    import scala.util.Try
+    ZIO.fromTry(Try(42/0))
+  }
+
+  // from Future
+  def createFromFuture(): Task[String] = {
+    import scala.concurrent.Future
+
+    lazy val future = Future.successful("Hello!")
+
+    ZIO.fromFuture { implicit ec =>
+      future.map(_ => "Goodbye!")
+    }
+  }
 
 }
